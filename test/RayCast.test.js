5852497140fdd5d7631d12c82c6ff6896b74886c
daniel.derevjanik <daniel.derevjanik@gmail.com>
2016-12-26 18:43:36 +0100

    added test for bigger map

--- test/RayCast.test.js
+++ test/RayCast.test.js
@@ -42,4 +42,40 @@ describe('castRay()', () => {
 
     });
 
+    describe('on blank map 12x12', () => {
+
+        it('should cast a ray from position [9, 1] to south', () => {
+            const castedRay = rayCast.castRay(testmaps.map12x12, 9, 1, helper.testUntilHit, enums.EDirection.SOUTH);
+            assert.equal(castedRay.x, 9);
+            assert.equal(castedRay.y, 9);
+            assert.equal(castedRay.dist, 8);
+            assert.equal(castedRay.row, 10);
+            assert.equal(castedRay.column, 8);
+        });
+
+        it('should cast a ray from position [10, 8] to east', () => {
+            const castedRay = rayCast.castRay(testmaps.map12x12, 10, 8, helper.testUntilHit, enums.EDirection.EAST);
+            assert.equal(castedRay.x, 10);
+            assert.equal(castedRay.y, 8);
+            assert.equal(castedRay.row, 7);
+            assert.equal(castedRay.column, 11);
+        });
+
+        it('should cast a ray from position [4, 3] to north', () => {
+            const castedRay = rayCast.castRay(testmaps.map12x12, 4, 3, helper.testUntilHit, enums.EDirection.EAST);
+            assert.equal(castedRay.x, 10);
+            assert.equal(Math.ceil(castedRay.y), 3);
+            assert.equal(castedRay.row, 2);
+            assert.equal(castedRay.column, 11);
+        });
+
+        it('shoudl cast a ray from position [6, 7] to west', () => {
+            const castedRay = rayCast.castRay(testmaps.map12x12, 6, 7, helper.testUntilHit, enums.EDirection.WEST);
+            assert.equal(castedRay.x, 1);
+            assert.equal(castedRay.y, 7);
+            assert.equal(castedRay.row, 8);
+            assert.equal(castedRay.column, 0);
+        });
+    });
+
 });
